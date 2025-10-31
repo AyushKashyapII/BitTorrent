95577334cc79163b989d5f79de964467da9fd29b
Ayush KAshyap <kashyap11ayush02@gmail.com>
2025-10-31 23:11:11 +0530

    making second commit

--- main.go
+++ main.go
@@ -2,11 +2,29 @@ package main
 
 import (
 	"fmt"
-	"bufio"
-	"strings"
+	"os"
 	"torrent/p2p"
 )
 
 func main(){
 	fmt.Println("BitTorrent .. ..")
+	if len(os.Args)<2{
+		fmt.Println("Usage: <torrent file>")
+		return 
+	}
+
+	torrentPath:=os.Args[1]
+	file,err:=os.Open(torrentPath)
+	if err!=nil{
+		fmt.Println("Error opeing torrent file")
+		return
+	}
+	defer file.Close()
+
+	tf,err:=p2p.Open(file)
+	if err!=nil{
+		fmt.Printf("Error parsing torrent file",err)
+		return
+	}
+	fmt.Println("Torrent file:",tf)
 }
